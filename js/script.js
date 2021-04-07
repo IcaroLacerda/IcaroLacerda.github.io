f960ee1e7d417efe4c49f76cb2e9f6e7eb63a32d
Kamikaze184 <80986182+IcaroLacerda@users.noreply.github.com>
2021-04-07 16:31:39 -0300

    placas de video e celulares

--- js/script.js
+++ js/script.js
@@ -9,15 +9,3 @@ span.addEventListener('click', function () {
   }
 })
 
-// const conteudo_drop = document.getElementById("conteudo-drop")
-// const promoçoes = document.getElementById("promoçoes")
-
-// const processador = document.getElementById("processador")
-// const placadevideo = document.getElementById("placadevideo")
-// const memoria = document.getElementById("memoria")
-
-
-// processador.addEventListener('click', function () {
-//     promoçoes.style.display = "none"
-// })
-
